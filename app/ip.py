7151f9b9dd4eb8c8a65e5a9b660ab60272a7fb19
chick_0 <chick_0@ch1ck.xyz>
2021-07-11 02:25:31 +0900

    add ip hash module

--- app/ip.py
+++ app/ip.py
@@ -1,2 +1,24 @@
-# get ip
-# get ip hash
+from secrets import token_bytes
+from hashlib import sha384
+
+
+def get_salt() -> str:
+    def gen_salt() -> str:
+        return token_bytes(20).hex()
+
+    # try to get salt from database
+
+    # on fail
+    salt = gen_salt()
+
+    return salt
+
+
+def get_ip_hash() -> str:
+    def get_ip() -> str:
+        return ""
+
+    ip = get_ip()
+    salt = get_salt()
+
+    return sha384(f"{ip}+{salt}".encode()).hexdigest()
