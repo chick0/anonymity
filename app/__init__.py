ccdd01d81e33a16cfcbeca908176ae322b7f8f5b
chick_0 <chick_0@ch1ck.xyz>
2021-07-11 02:25:16 +0900

    add template_filter

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,10 @@ def create_app():
     for view in views.__all__:
         app.register_blueprint(getattr(views, view).__getattribute__("bp"))
 
+    from . import template_filter
+    for name in template_filter.filter_list:
+        app.add_template_filter(f=getattr(template_filter, name), name=name)
+
     from . import models
     db.init_app(app=app)
     migrate.init_app(app=app, db=db)
