17a32cbc7128207421f1fe8391801e7e23ddc9af
chick_0 <chick_0@ch1ck.xyz>
2021-07-11 22:17:40 +0900

    reply blueprint

--- app/views/reply.py
+++ app/views/reply.py
@@ -4,18 +4,67 @@ from flask import request
 from flask import jsonify
 
 
+from app import db
+from app.models import Board
+from app.models import Reply
+
+
 bp = Blueprint(
     name="reply",
     import_name="reply",
     url_prefix="/reply"
 )
 
-# To-Do
-# 1) append reply to database
+
+@bp.get("/")
+def add():
+    board_idx = request.args.get("board_idx", -1, type=int)
+    board = Board.query.filter_by(
+        idx=board_idx
+    ).first()
+    if board is None:
+        return jsonify({
+            "message": "board not found",
+            "result": "failed"
+        }), 404
+
+    reply = Reply()
+    reply.board_idx = board_idx
+    reply.content = request.form.get("content")
+    reply.good, reply.bad = 0, 0
+
+    db.session.add(reply)
+    db.session.commit()
+    return jsonify({
+        "idx": reply.idx,
+        "result": "ok"
+    })
 
 
-# To-Do
-# 1) get reply from database
+@bp.route("/<int:idx>")
+def get(idx: int):
+    reply_list = Reply.search.filter_by(
+        board_idx=idx
+    ).order_by(Reply.idx.desc()).paginate(page=request.args.get("page", 1, type=int))
+
+    result = []
+    for reply in reply_list.items:
+        result.append({
+            "idx": reply.idx,
+            "content": reply.content,
+            "date": reply.date,
+            "good": reply.good,
+            "bad": reply.bad
+        })
+
+    return jsonify({
+        "head": {
+            "board_idx": idx,
+            "page": reply_list.page,
+            "total_page": reply_list.pages
+        },
+        "body": result
+    })
 
 
 # To-Do
