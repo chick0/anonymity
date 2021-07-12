68234fe1a23d7767d4c258758aa9dbc6c4a64e1e
chick_0 <chick_0@ch1ck.xyz>
2021-07-13 08:55:56 +0900

    delete unused filter

--- app/template_filter.py
+++ app/template_filter.py
@@ -1,29 +1,3 @@
-def link_old(html: str):
-    from re import compile
-    pattern = compile(r"\[(.*?)\]\((.*?)\)")
-
-    a_tags = []
-    for tag in pattern.findall(html):
-        a_tags.append({
-            "text": tag[0],
-            "href": tag[1],
-            "target": "_blank",
-            "rel": "noreferrer"
-        })
-
-    return a_tags
-
-
-def link(html: str):
-    from re import compile
-    pattern = compile(r"\[(.*?)\]\((.*?)\)")
-
-    for tag in pattern.findall(html):
-        print(tag)
-
-    return html
-
-
 # do not touch this
 filter_list = [name for name in dir() if not name.startswith("_")]
 # do not touch this
