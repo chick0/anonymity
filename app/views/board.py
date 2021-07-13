3403976876d0a789451d6d45cfdb07d8549429cb
chick_0 <chick_0@ch1ck.xyz>
2021-07-13 20:12:41 +0900

    add page button

--- app/views/board.py
+++ app/views/board.py
@@ -1,6 +1,7 @@
 
 from flask import Blueprint
 from flask import request
+from flask import url_for
 from flask import render_template
 
 from app.models import Board
@@ -17,9 +18,18 @@ bp = Blueprint(
 def show_all():
     board = Board.query.order_by(
         Board.idx.desc()
-    ).paginate(page=request.args.get("page", 1, type=int))
+    ).paginate(page=request.args.get("page", 1, type=int),
+               max_per_page=8)
+
+    prev_page = url_for("board.show_all", page=board.prev_num) \
+        if board.prev_num is not None else "javascript:alert('page not found')"
+
+    next_page = url_for("board.show_all", page=board.next_num) \
+        if board.next_num is not None else "javascript:alert('page not found')"
 
     return render_template(
         "board/show_all.html",
-        board_list=board
+        board_list=board,
+        prev=prev_page,
+        next=next_page
     )
