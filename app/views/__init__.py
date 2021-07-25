866b6824837b083152fa147473a0afd259c65fb6
chick_0 <chick_0@ch1ck.xyz>
2021-07-25 17:40:32 +0900

    `__` -> `_`

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -3,7 +3,7 @@ from os import listdir
 
 __all__ = [
     view_name[:-3]
-    for view_name in listdir(path.dirname(__file__)) if not view_name.startswith("__")
+    for view_name in listdir(path.dirname(__file__)) if not view_name.startswith("_")
 ]
 
 
