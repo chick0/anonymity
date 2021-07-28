280882fba689fe69fb98d4f8c845c6dcf2ba367c
chick_0 <chick_0@ch1ck.xyz>
2021-07-28 21:53:37 +0900

    typo

--- app/__init__.py
+++ app/__init__.py
@@ -14,17 +14,21 @@ APP_NAME = "Anonymity"
 def create_app():
     app = Flask(__name__)
 
+    # load config
     from . import config
     app.config.from_object(config)
 
+    # blueprint init
     from . import views
     for view in views.__all__:
         app.register_blueprint(getattr(views, view).__getattribute__("bp"))
 
+    # template filter init
     from . import template_filter
     for name in template_filter.filter_list:
         app.add_template_filter(f=getattr(template_filter, name), name=name)
 
+    # database init
     from . import models
     db.init_app(app=app)
     migrate.init_app(app=app, db=db)
