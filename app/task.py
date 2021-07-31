35c32ee957e08b4b869cd92a6dd98e176d34329d
chick_0 <chick_0@ch1ck.xyz>
2021-08-01 00:18:17 +0900

    background task update

--- app/task.py
+++ app/task.py
@@ -1,6 +1,8 @@
+from time import time
 from time import sleep
 from datetime import datetime
 from datetime import timedelta
+from threading import Thread
 
 from app import db
 from app.models import Salt
@@ -10,45 +12,87 @@ from app.models import Recommend
 from app.ip import gen_salt
 
 
-def loop(app) -> None:
-    while True:
-        with app.app_context():
-            # 1) check the ip salt
-            salt = Salt.query.first()
-            if salt is None:
-                gen_salt()
-            else:
-                if datetime.now() >= salt.created + timedelta(days=1):
-                    db.session.delete(salt)
-                    db.session.commit()
-                    gen_salt()
+def core(app):
+    task_list = [
+        {
+            "time": time(),
+            "target": salt_lifetime,   # 2h
+            "call": 2 * 60 * 60
+        },
+        {
+            "time": time(),
+            "target": vote_updater,    # 2m
+            "call": 2 * 60
+        },
+        {
+            "time": time(),
+            "target": token_cleaner,   # 5m
+            "call": 5 * 60
+        },
+    ]
 
-            # 2) Collect Voting Results
-            # 2-1) Board
-            votes = Recommend.query.filter_by(
-                is_board=True,
-                used=False
-            ).all()
-            for vote in votes:
+    while True:
+        now = time()
+        for target_id, task_target in enumerate(task_list):
+            time_flow = int(now - task_target['time'])
+
+            if time_flow >= task_target['call']:
+                Thread(
+                    target=task_target['target'],
+                    args=(app,)
+                ).start()
+
+                # timer reset
+                task_list[target_id]['time'] = now
+
+        sleep(30)
+
+
+def salt_lifetime(app):
+    with app.app_context():
+        salt = Salt.query.first()
+        if salt is None:
+            gen_salt()
+        else:
+            if datetime.now() >= salt.created + timedelta(days=1):
+                db.session.delete(salt)
+                gen_salt()
+
+
+def vote_updater(app):
+    with app.app_context():
+        board_cache = {}
+        votes = Recommend.query.filter_by(
+            is_board=True,
+            used=False
+        ).limit(200).all()
+        for vote in votes:
+            board = board_cache.get(vote.target_idx, None)
+            if board is None:
                 board = Board.query.filter_by(
                     idx=vote.target_idx
                 ).first()
-                if board is not None:
-                    if vote.vote is True:
-                        board.good += 1
-                    else:
-                        board.bad += 1
+                board_cache[vote.target_idx] = board
 
-                    vote.used = True
+            if board is not None:
+                if vote.vote is True:
+                    board.good += 1
                 else:
-                    db.session.delete(vote)
+                    board.bad += 1
 
-            db.session.commit()
+                # save vote result
+                vote.used = True
+            else:
+                # if the target is empty, delete the vote result
+                db.session.delete(vote)
 
-            # 3) Remove Expired Sessions
-            for token in Token.query.all():
-                if not token.expire >= datetime.now():
-                    db.session.delete(token)
-            db.session.commit()
+        db.session.commit()
 
-        sleep(35)
+
+def token_cleaner(app):
+    with app.app_context():
+        for token in Token.query.all():
+            if not token.expire >= datetime.now():
+                db.session.delete(token)
+
+        db.session.commit()
